adce555801423d696053aa2becc71b55c9aabde1
Pallavi Jain <jainpallavi717@gmail.com>
2026-02-22 00:40:32 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a = 25;
+char grade = 'A';
+float pi = 3.14;
+cout<< pi << endl;
+return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char grade = 'A';
+    bool isspace = false;
+    cout<<isspace<<endl;
+    return 0;
+}
