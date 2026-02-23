b383d9d23aac56441d5064d9370940373ed45348
Pallavi Jain <jainpallavi717@gmail.com>
2026-02-23 16:58:11 +0530

    Add files via upload

---
+++ condition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter n : "<<endl;
+    cin >> n;
+    if(n>0){
+        cout<<"positive number"<<endl;
+    }
+    else{
+        cout<<"negative number"<<endl;
+    }
+    return 0;
+}
